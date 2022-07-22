971d1371156e48e7c09b9da1aec31198cfad2384
Filixo <alaeboughaba@gmail.com>
2022-07-22 16:50:30 +0200

    ajout de classe Boutique dans jeu

--- src/main/java/metier/Jeu.java
+++ src/main/java/metier/Jeu.java
@@ -6,10 +6,11 @@ public class Jeu {
 
 	private String titre;
 	private Console console;
+	private Boutique boutique;
 	
 	
 	
-	public Jeu(String titre, Console console) {
+	public Jeu(String titre, Console console, Boutique boutique) {
 		this.titre = titre;
 		this.console = console;
 	}
@@ -40,9 +41,21 @@ public class Jeu {
 
 
 
+	public Boutique getBoutique() {
+		return boutique;
+	}
+
+
+
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "Jeu [titre=" + titre + ", console=" + console + "]";
+		return "Jeu [titre=" + titre + ", console=" + console + ", boutique=" + boutique + "]";
 	}
 	
 	
